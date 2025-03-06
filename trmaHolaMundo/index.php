3b3c233570211734b848956ab2b692026e966157
MickVillegas <mickprofesional@gmail.com>
2025-03-06 10:51:15 +0100

    Add files via upload

--- trmaHolaMundo/index.php
+++ trmaHolaMundo/index.php
@@ -1,3 +1,17 @@
 <?php get_header()?>
     <p>Hola Mundo</p>
+
+    <?php 
+        if(have_posts()): 
+            while(have_posts()): the_post():
+    ?>
+
+                <p><?php the_title()?></p>
+
+    <?php             
+            endwhile;
+        else:
+            __('pito', 'Por mick');
+        endif;?>
+
 <?php get_footer(); ?>
